9c0bed268228d7dbd32e89227eac8db8291ad353
crislainemlima1 <145732631+crislainemlima1@users.noreply.github.com>
2026-07-09 21:48:55 -0300

    feat: implementando o painel de forma correta

--- focusflow-react/src/App.jsx
+++ focusflow-react/src/App.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Painel from './pages/Painel';
+import Dashboard from './components/Dashboard';
+import PomodoroTimer from './components/PomodoroTimer';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/painel/*" element={<Painel />} />
+        <Route path="/painel/*" element={<Painel />}>
+          <Route index element={<Dashboard />} />
+          <Route path="timer" element={<PomodoroTimer />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
